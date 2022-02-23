10dea5fabd3c071b04ce4fd399bbab4afbf6cf3d
unknown <sonubassam.00@gmail.com>
2022-02-23 10:52:27 +0530

    Search UI completed

--- lib/presentation/search/widgets/search_idle.dart
+++ lib/presentation/search/widgets/search_idle.dart
@@ -1,17 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:netflix/core/constants.dart';
+import 'package:netflix/presentation/search/widgets/title.dart';
 
+ final imgageUrl = 
+    "https://www.themoviedb.org/t/p/w500_and_h282_face/4OTYefcAlaShn6TGVK33UxLW9R7.jpg"
+  ;
 class SearchIdleWidget extends StatelessWidget {
-  const SearchIdleWidget({ Key? key }) : super(key: key);
+ const  SearchIdleWidget({Key? key}) : super(key: key);
+ 
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: const [
-       Text("Top Searches",
-         style: TextStyle(
-           fontSize: 22,
-           fontWeight: FontWeight.bold
-         ),),
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children:  [
+       Heading(title: "Top Searches"),
+        kheight,
+        Expanded(
+          child: ListView.separated(
+            shrinkWrap: true,
+            itemBuilder: (ctx,index)=>const TopSearchTile(),
+           separatorBuilder: (ctx,index)=>kheight,
+           itemCount: 10),
+        )
+      ],
+    );
+  }
+}
+
+
+class TopSearchTile extends StatelessWidget {
+  const TopSearchTile({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final screenwidth = MediaQuery.of(context).size.width;
+    return Row(
+      children: [
+        Container(
+          width: screenwidth *0.35,
+          height: 70,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: NetworkImage(imgageUrl))
+          ),
+        ),
+        const Expanded(child: Padding(
+          padding: EdgeInsets.only(left: 6),
+          child: Text("The KingsMan",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16
+          ),),
+        )),
+      const  Icon(Icons.play_circle_fill,
+        size: 40,
+        color: Colors.grey,)
       ],
       
     );
---
+++ lib/presentation/search/widgets/search_result.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:netflix/core/constants.dart';
+import 'package:netflix/presentation/search/screen_search.dart';
+import 'package:netflix/presentation/search/widgets/title.dart';
+
+class SearchResultWidget extends StatelessWidget {
+  const SearchResultWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Heading(title: "Movies & TV"),
+        kheight,
+        Expanded(
+            child: GridView.count(
+                crossAxisCount: 3,
+                mainAxisSpacing: 5,
+                crossAxisSpacing: 5,
+                childAspectRatio: 1/1.4,
+                shrinkWrap: true,
+                children: List.generate(20, (index) {
+                  return MainCard();
+                })))
+      ],
+    );
+  }
+}
+class MainCard extends StatelessWidget {
+  const MainCard({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration:  BoxDecoration(
+        image: const DecorationImage(
+          fit: BoxFit.cover,
+          image: NetworkImage(imagecard)),
+         borderRadius:BorderRadius.circular(7) ),
+    
+    );
+  }
+}
